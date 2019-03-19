f3c0b88018831ce65db293c17f0279b4bb09a519
konradslowik <conxplay@gmail.com>
2019-03-19 19:25:14 +0100

    added sprites and box coliders

--- New Unity Project (2)/Assets/LevelManager.cs
+++ New Unity Project (2)/Assets/LevelManager.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name)
 	{
 		Debug.Log ("Level load requested for: "+name);
-		Application.LoadLevel(name);
+        //Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
 	}
 
 	public void QuitRequest()
